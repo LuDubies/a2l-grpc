0e4e9de0afb6edb9245854434f8b302905633400
Dubies Luca <luca.dubies@newtec.de>
2023-09-28 17:08:24 +0200

    restoring workflow, adding extended test for showcase

--- cmd/a2l/a2l_test.go
+++ cmd/a2l/a2l_test.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"testing"
+
 	"github.com/sauci/a2l-grpc/pkg/a2l"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func baseTypeToPointer[T bool | uint32 | string](baseType T) *T {
@@ -182,6 +183,27 @@ func TestGrpcA2LImplType_GetA2LFromTree(t *testing.T) {
 										UpperLimit:     &a2l.FloatType{Value: 0.0},
 									},
 								},
+								UNIT: []*a2l.UnitType{
+									{
+										Name:           &a2l.IdentType{Value: "someunit"},
+										LongIdentifier: &a2l.StringType{Value: "_"},
+										Display:        &a2l.StringType{Value: "_"},
+										Type:           "idkman",
+									},
+								},
+								MEASUREMENT: []*a2l.MeasurementType{
+									{
+										Name:           &a2l.IdentType{Value: "measurement"},
+										LongIdentifier: &a2l.StringType{Value: "_"},
+										DataType:       &a2l.DataTypeType{Value: "UWORD"},
+										Conversion:     &a2l.IdentType{Value: "conv"},
+										Resolution:     &a2l.IntType{Value: 0},
+										Accuracy:       &a2l.FloatType{Value: 0.0},
+										LowerLimit:     &a2l.FloatType{Value: 0.0},
+										UpperLimit:     &a2l.FloatType{Value: 0.0},
+										LAYOUT:         &a2l.LayoutType{IndexMode: "COL_DIR"},
+									},
+								},
 							},
 						},
 					},
@@ -202,6 +224,10 @@ func TestGrpcA2LImplType_GetA2LFromTree(t *testing.T) {
   /end CHARACTERISTIC
   /begin CHARACTERISTIC c[1] "_" VALUE 0x0 _ 0 _ 0 0
   /end CHARACTERISTIC
+  /begin MEASUREMENT measurement "_" UWORD conv 0x0 0 0 0
+  /end MEASUREMENT
+  /begin UNIT someunit "_" "_" idkman
+  /end UNIT
  /end MODULE
 /end PROJECT`),
 				Error: nil,
@@ -218,4 +244,81 @@ func TestGrpcA2LImplType_GetA2LFromTree(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_GetTreeFromA2L(t *testing.T) {
+	type testCaseType struct {
+		request  *a2l.TreeFromA2LRequest
+		response *a2l.TreeResponse
+	}
+
+	tc := testCaseType{
+		request: &a2l.TreeFromA2LRequest{
+			A2L: []byte(`/begin PROJECT _ "_"
+			/begin MODULE _ "_"
+			 /begin CHARACTERISTIC a[0] "_" VALUE 0x0 _ 0 _ 0 0
+			 /end CHARACTERISTIC
+			 /begin MEASUREMENT measurement "_" UWORD conv 0x0 0 0 0 LAYOUT COLUMN_DIR
+  			 /end MEASUREMENT
+			/end MODULE
+		   /end PROJECT`),
+		},
+		response: &a2l.TreeResponse{
+			Tree: &a2l.RootNodeType{
+				PROJECT: &a2l.ProjectType{
+					Name:           &a2l.IdentType{Value: "_"},
+					LongIdentifier: &a2l.StringType{Value: "_"},
+					MODULE: []*a2l.ModuleType{
+						{
+							Name:           &a2l.IdentType{Value: "_"},
+							LongIdentifier: &a2l.StringType{Value: "_"},
+							CHARACTERISTIC: []*a2l.CharacteristicType{
+								{
+									Name:           &a2l.IdentType{Value: "a[0]"},
+									LongIdentifier: &a2l.StringType{Value: "_"},
+									Type:           "VALUE",
+									Address: &a2l.LongType{
+										Value: 0x00000000,
+										Base:  16,
+										Size:  1},
+									Deposit:    &a2l.IdentType{Value: "_"},
+									MaxDiff:    &a2l.FloatType{Value: 0.0},
+									Conversion: &a2l.IdentType{Value: "_"},
+									LowerLimit: &a2l.FloatType{Value: 0.0},
+									UpperLimit: &a2l.FloatType{Value: 0.0},
+								},
+							},
+							MEASUREMENT: []*a2l.MeasurementType{
+								{
+									Name:           &a2l.IdentType{Value: "measurement"},
+									LongIdentifier: &a2l.StringType{Value: "_"},
+									DataType:       &a2l.DataTypeType{Value: "UWORD"},
+									Conversion:     &a2l.IdentType{Value: "conv"},
+									Resolution: &a2l.IntType{
+										Value: 0,
+										Base:  16,
+										Size:  1},
+									Accuracy:   &a2l.FloatType{Value: 0.0},
+									LowerLimit: &a2l.FloatType{Value: 0.0},
+									UpperLimit: &a2l.FloatType{Value: 0.0},
+									LAYOUT:     &a2l.LayoutType{IndexMode: "COL_DIR"},
+								},
+							},
+						},
+					},
+				},
+			},
+			Error: nil,
+		},
+	}
+
+	t.Run("", func(t *testing.T) {
+		grpc := grpcA2LImplType{}
+
+		if response, err := grpc.GetTreeFromA2L(nil, tc.request); err == nil {
+			assert.Equal(t, tc.response, response)
+		} else {
+			t.Fatal(err)
+		}
+	})
 }
